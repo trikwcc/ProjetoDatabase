34f0e5669a37a7d623c9395fe7c96896b48e2df5
Pogeku <127849816+FaLnOrder@users.noreply.github.com>
2023-10-09 02:36:40 -0300

    updt

--- BancodeDados/DatabaseProgram/application/Controller.java
+++ BancodeDados/DatabaseProgram/application/Controller.java
@@ -1,13 +1,11 @@
 package application;
-import application.Database;
-
-
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
+
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -16,7 +14,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
@@ -46,53 +43,7 @@ public class Controller implements Initializable {
     private Connection connect;
     private PreparedStatement prepared;
     private ResultSet result;
-    
-<<<<<<< Updated upstream
-    public void loginAdmin() throws IOException {
-    	try {
-			prepare = connect.prepareStatement(sql);
-			prepare.setString(1,username.getText());
-			prepare.setString(2,password.getText());
-			
-			result = prepare.executeQuery();
-			
-			Alert alert;
-			
-			if(username.getText().isEmpty() || password.getText().isEmpty()) {
-				alert = new Alert(AlertType.ERROR);
-				alert.setTitle("Error Message");
-				alert.setHeaderText(null);
-				alert.setContentText("Please fill all blank fields");
-				
-			} else {
-				if(result.next()) {
-					alert = new Alert(AlertType.INFORMATION);
-					alert.setTitle("Information Message");
-					alert.setHeaderText(null);
-					alert.setContentText("Sucessfull Login");
-					
-					loginBtch.getScene().getWindow().hide();
-					Parent root = FXMLLoader.load(getClass().getResource("Teladealter.fxml"));
-					Stage stage = new Stage();
-					Scene scene = new Scene(root);
-					stage.setScene(scene);
-					stage.show();
-					
-				} else {
-					alert = new Alert(AlertType.ERROR);
-					alert.setTitle("Error Message");
-					alert.setHeaderText(null);
-					alert.setContentText("Wrong username/password");
-				}
-			}
-			
-			
-			
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-    	
-=======
+   
     private double x = 0;
     private double y = 0;
 
@@ -154,7 +105,6 @@ public class Controller implements Initializable {
             
         }catch(Exception e){e.printStackTrace();}
         
->>>>>>> Stashed changes
     }
     
     public void close(){
---
+++ aporlx/src/module-info.java
@@ -0,0 +1,5 @@
+module aporlx {
+	requires javafx.controls;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
